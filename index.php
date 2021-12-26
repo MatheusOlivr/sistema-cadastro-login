2c23a311d2a4a8fb3c0f3ff51232e079af18d504
MatheusOlivr <contato.matheusoliv@gmail.com>
2021-12-26 15:53:39 -0300

    Cria e estiliza a página inicial do projeto

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>PAGINA INICIAL</title>
+	<link rel = "stylesheet" href = "css/style.css">
+</head>	
+<body>
+	<div id = 'box'>
+		<a class = 'box-item' href = "login.php">Login</a>
+		<a class = 'box-item' href = "cadastro.php">Cadastre-se</a>
+	</div>
+</body>
+</html>
